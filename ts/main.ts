670e6d6b6733bbb709b6395bc55626675f06dd43
Scott <greatvstech@gmail.com>
2024-04-24 13:41:13 -0700

    Created starting book.html and a Book class

---
+++ ts/main.ts
@@ -0,0 +1,18 @@
+/**
+ * Represents a individual book that can be purchased
+ */
+class Book {
+    isbn : string;
+    title : string;
+    price : number;
+    releaseDate : Date;
+}
+
+// Book object test code
+let myBook = new Book();
+myBook.isbn = "123";
+myBook.price = 9.99;
+myBook.title = "Programming for beginners";
+myBook.releaseDate = new Date(2023, 9, 8); // Months start at index 0
+
+console.log(myBook);
