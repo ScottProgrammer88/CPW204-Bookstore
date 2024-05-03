b9a8ce4ebb44a3640f09fef29c24b34255d0d968
Scott <greatvstech@gmail.com>
2024-05-02 17:40:32 -0700

    Creating function stubs for the add book form

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ myBook.price = 9.99;
 myBook.title = "Programming for beginners";
 myBook.releaseDate = new Date(2023, 9, 8);
 console.log(myBook);
+window.onload = function () {
+    let addBookBtn = document.querySelector("#add-book");
+    addBookBtn.onclick = processBook;
+};
+function processBook() {
+    console.log("processBook was called");
+    let userBook = getBook();
+    if (userBook != null) {
+        addBook(userBook);
+    }
+}
+function getBook() {
+    return null;
+}
+function addBook(b) {
+}
--- ts/main.ts
+++ ts/main.ts
@@ -32,3 +32,38 @@ myBook.title = "Programming for beginners";
 myBook.releaseDate = new Date(2023, 9, 8); // Months start at index 0
 
 console.log(myBook);
+
+
+window.onload = function() {
+        // Set up button click for add book form
+        let addBookBtn = document.querySelector("#add-book") as HTMLButtonElement;
+        addBookBtn.onclick = processBook;
+}
+
+function processBook() {
+    console.log("processBook was called");
+
+    let userBook = getBook();
+    if (userBook != null) {
+        addBook(userBook);
+    }
+}
+
+/**
+ * This function will retrieve all the book 
+ * data from the HTML page. If all data is valid
+ * a Book object will be returned. If any data
+ * is invalid, null will be returned 
+ * @returns 
+ */
+function getBook():Book {
+    return null;
+}
+
+/**
+ * Adds a Book object to web storage. Assumes all data is valid
+ * @param b The Book containing valid data to be added
+ */
+function addBook(b:Book):void {
+
+}
