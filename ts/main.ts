f86eb934a941e85c8e87fdad18b5dd6d759d226d
Scott <greatvstech@gmail.com>
2024-05-21 14:20:15 -0700

    Commented out unneeded test code

--- ts/main.ts
+++ ts/main.ts
@@ -24,14 +24,14 @@ class Book {
     releaseDate : Date;
 }
 
-// Book object test code
+/* // Book object test code
 let myBook = new Book();
 myBook.isbn = "123";
 myBook.price = 9.99;
 myBook.title = "Programming for beginners";
 myBook.releaseDate = new Date(2023, 9, 8); // Months start at index 0
 
-console.log(myBook);
+console.log(myBook); */
 
 
 window.onload = function() {
